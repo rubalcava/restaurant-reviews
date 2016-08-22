da5876808794b05681a490417af257dafe3b2c0b
rubalcava <radgokart@gmail.com>
2016-08-22 11:21:31 -0700

    chore: Beautified code with jsbeautifier.org

--- src/js/index.js
+++ src/js/index.js
@@ -16,12 +16,12 @@ var ViewModel = function() {
         var entire_tip_section = '';
 
         $.ajax({
-    		url: 'https://api.foursquare.com/v2/venues/' + place_id + '/tips?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815',
-    		dataType: 'json',
-    		success: function(response) {
+            url: 'https://api.foursquare.com/v2/venues/' + place_id + '/tips?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815',
+            dataType: 'json',
+            success: function(response) {
                 if (response.response.tips.count > 0) {
                     /* loop through each tip */
-                    for (var i=0; i< response.response.tips.count; i++) {
+                    for (var i = 0; i < response.response.tips.count; i++) {
                         user_first_name = response.response.tips.items[i].user.firstName;
                         /* check is last name exists */
                         if (response.response.tips.items[i].user.lastName !== undefined) {
@@ -29,7 +29,7 @@ var ViewModel = function() {
                         }
 
                         review_text = response.response.tips.items[i].text;
-                        entire_tip_section = entire_tip_section + '<p><strong>'+ user_first_name + ' ' + user_last_name + ':</strong></p>' + '<p>' + review_text + '</p>';
+                        entire_tip_section = entire_tip_section + '<p><strong>' + user_first_name + ' ' + user_last_name + ':</strong></p>' + '<p>' + review_text + '</p>';
 
 
                         user_first_name = '';
@@ -48,12 +48,12 @@ var ViewModel = function() {
                 else {
                     document.getElementById('subresult-img').innerHTML = '<p tabindex="0"><strong>Tips: </strong></p>' + '<p>No user tips found.</p>';
                 }
-    		},
+            },
             error: function(err) {
                 console.log(err);
                 document.getElementById('subresult-img').innerHTML = '<p><strong>Tips: </strong></p>' + '<p><strong>Error loading tips from the Foursquare API.<br>Please try again later.</strong></p>';
             }
-    	});
+        });
     }
 
     function getImage(place_id, place_name) {
@@ -61,9 +61,9 @@ var ViewModel = function() {
         var image_location = '';
         var image_alt = '';
         $.ajax({
-    		url: 'https://api.foursquare.com/v2/venues/' + place_id + '/photos?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815',
-    		dataType: 'json',
-    		success: function(response) {
+            url: 'https://api.foursquare.com/v2/venues/' + place_id + '/photos?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815',
+            dataType: 'json',
+            success: function(response) {
                 if (response.response.photos.items.length > 0) {
                     image_location = response.response.photos.items[0].prefix + response.response.photos.items[0].height + 'x' + response.response.photos.items[0].width + response.response.photos.items[0].suffix;
 
@@ -73,19 +73,18 @@ var ViewModel = function() {
                     document.getElementById('subresult-img').innerHTML = '<img class="img-responsive" ' + 'src="#" ' + 'alt="No image found in Foursquare API"' + '>';
                 }
 
-    		}
-    	});
+            }
+        });
     }
 
     function getHours(place_id) {
 
         /* ajax call for hours*/
         $.ajax({
-    		url: 'https://api.foursquare.com/v2/venues/' + place_id + '/hours?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815',
-    		dataType: 'json',
-    		success: function(response) {
-                Promise.resolve(response).then(function(the_response)
-                {
+            url: 'https://api.foursquare.com/v2/venues/' + place_id + '/hours?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815',
+            dataType: 'json',
+            success: function(response) {
+                Promise.resolve(response).then(function(the_response) {
                     self.data_for_hours.length = 0;
                     for (var object_index in the_response.response.hours.timeframes) {
                         var thing_to_search_in = the_response.response.hours.timeframes;
@@ -114,7 +113,10 @@ var ViewModel = function() {
                                 thing_to_search_in[object_index].days[day] = 'Sunday';
                             }
                         }
-                        self.data_for_hours.push({days: thing_to_search_in[object_index].days, hours: thing_to_search_in[object_index].open});
+                        self.data_for_hours.push({
+                            days: thing_to_search_in[object_index].days,
+                            hours: thing_to_search_in[object_index].open
+                        });
                     }
 
                 }).then(function() {
@@ -123,7 +125,13 @@ var ViewModel = function() {
                     var formatted_hours;
 
                     if (self.data_for_hours.length === 0) {
-                        self.data_for_hours.push({days: ['No  operating hours found'], hours: [{start: 'No opening time found', end: 'No closing time found'}]});
+                        self.data_for_hours.push({
+                            days: ['No  operating hours found'],
+                            hours: [{
+                                start: 'No opening time found',
+                                end: 'No closing time found'
+                            }]
+                        });
                     }
                     for (var item in self.data_for_hours) {
                         var current_item = self.data_for_hours[item];
@@ -145,17 +153,17 @@ var ViewModel = function() {
 
                         for (var hours_object_index = 0; hours_object_index < hours_array.length; hours_object_index++) {
                             formatted_hours = '';
-                            formatted_hours = formatted_hours + 'Open: ' + hours_array[hours_object_index].start + '<br>' + 'Close: ' + hours_array[hours_object_index].end+ '<br><br>';
+                            formatted_hours = formatted_hours + 'Open: ' + hours_array[hours_object_index].start + '<br>' + 'Close: ' + hours_array[hours_object_index].end + '<br><br>';
 
                             document.getElementById('subresult-hours').innerHTML = document.getElementById('subresult-hours').innerHTML + formatted_hours;
                         }
                     }
                 });
-    		},
+            },
             error: function(err) {
                 console.log(err);
             }
-    	});
+        });
     }
 
     function getSubResults(index) {
@@ -199,21 +207,23 @@ var ViewModel = function() {
         document.getElementById('subresult').style.display = 'none';
         /* initial ajax call to get results */
         $.ajax({
-    		url: 'https://api.foursquare.com/v2/venues/search?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815&near=' + location_to_search + '&query=' + cuisine_to_search_for,
-    		dataType: 'json',
-    		success: function(response) {
+            url: 'https://api.foursquare.com/v2/venues/search?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815&near=' + location_to_search + '&query=' + cuisine_to_search_for,
+            dataType: 'json',
+            success: function(response) {
                 self.data_from_model.removeAll();
 
-                if (response.response.venues.length < 1 ) {
-                    self.data_from_model.push({name: 'No results found', index: 0});
+                if (response.response.venues.length < 1) {
+                    self.data_from_model.push({
+                        name: 'No results found',
+                        index: 0
+                    });
 
                     document.getElementById('subresult-name').innerHTML = '';
                     document.getElementById('subresult-address').innerHTML = '';
                     document.getElementById('subresult-cuisine-type').innerHTML = '';
                     document.getElementById('subresult-img').innerHTML = '';
                     document.getElementById('subresult-hours').innerHTML = '';
-                }
-                else {
+                } else {
                     for (var i = 0; i < response.response.venues.length; i++) {
                         response.response.venues[i].index = 'results-list-item' + i;
                         response.response.venues[i].visibility = ko.observable(true);
@@ -223,7 +233,7 @@ var ViewModel = function() {
                             break;
                         }
                     }
-                    for (var j=0; j < self.data_from_model().length; j++) {
+                    for (var j = 0; j < self.data_from_model().length; j++) {
 
                         function theCloser(current_index) {
                             document.getElementById('results-list-item' + current_index).addEventListener('click', function() {
@@ -232,12 +242,12 @@ var ViewModel = function() {
 
                             /* code from http://stackoverflow.com/questions/155188/trigger-a-button-click-with-javascript-on-the-enter-key-in-a-text-box */
                             document.getElementById('results-list-item' + current_index)
-                            .addEventListener("keyup", function(event) {
-                                event.preventDefault();
-                                if (event.keyCode == 13) {
-                                    document.getElementById('results-list-item' + current_index).click();
-                                }
-                            });
+                                .addEventListener("keyup", function(event) {
+                                    event.preventDefault();
+                                    if (event.keyCode == 13) {
+                                        document.getElementById('results-list-item' + current_index).click();
+                                    }
+                                });
                         }
                         theCloser(j);
                     }
@@ -247,7 +257,7 @@ var ViewModel = function() {
                 document.getElementById('checkbox-is-chain').dispatchEvent(event);
 
                 document.getElementById('checkbox-users-there').dispatchEvent(event);
-    		},
+            },
             error: function(err) {
                 console.log(err);
                 self.data_from_model.removeAll();
@@ -257,7 +267,7 @@ var ViewModel = function() {
                 document.getElementById('subresult-img').innerHTML = '';
                 document.getElementById('subresult-hours').innerHTML = '';
             }
-    	});
+        });
     });
 
     document.getElementById('checkbox-is-chain').addEventListener('change', function() {
